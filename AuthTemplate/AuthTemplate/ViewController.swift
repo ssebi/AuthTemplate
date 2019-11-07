5538dc105596d5d8b8412e13919588acfe8d3506
Sebastian Vidrea <vidreasebastian@gmail.com>
2019-11-07 17:12:25 +0200

    Initial commit. Added basic structure.

---
+++ AuthTemplate/AuthTemplate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AuthTemplate
+//
+//  Created by Sebastian Vidrea on 07/11/2019.
+//  Copyright © 2019 AppCompany. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
